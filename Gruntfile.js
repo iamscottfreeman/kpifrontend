9ab4d29866ee6906a1970f55bf620965fc348a4b
Scott Freeman <email@scottfreeman.net>
2015-05-14 15:56:00 +0100

    Update Bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -114,7 +114,6 @@ module.exports = function (grunt) {
         reporter: require('jshint-stylish')
       },
       all: [
-        'Gruntfile.js',
         '<%= config.app %>/scripts/{,*/}*.js',
         '!<%= config.app %>/scripts/vendor/*',
         'test/spec/{,*/}*.js'
